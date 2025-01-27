b066ed7f364fca3f8082550375aa3fc8ead7d7d3
Pedro Batista <pedroguimabatista@gmail.com>
2025-01-27 16:26:49 -0300

    third problem

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,9 @@
 # How many new outputs were created by block 123,456?
+
+total_outputs=0
+for txid in $txids; do
+  outputs=$(C:\Program Files\Bitcoin\daemon\bitcoin-cli.exe -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction "$txid" 1 | jq '.vout | length')
+  total_outputs=$((total_outputs + outputs))
+done
+echo "Total outputs in block 123,456: $total_outputs"
+
