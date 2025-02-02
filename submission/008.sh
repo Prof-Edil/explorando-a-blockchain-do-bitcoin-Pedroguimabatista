f36955997551a9f3c68ad5545921acbb45fe77c1
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-01 21:31:11 -0300

    Eight problem - Second try

--- submission/008.sh
+++ submission/008.sh
@@ -1,8 +1,8 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
-#!/usr/bin/env bash
 
-tx_data=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1)
-pubkey=$(echo "$tx_data" | jq -r '.vin[0].txinwitness[1]')
-echo "$pubkey"
+rawtx=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1)
+npubencodeed=$(echo $rawtx | jq -r '.vin[0].txinwitness[2]')
+decode=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" decodescript $npubencodeed)
+echo $decode | jq -r '.asm' | cut -d ' ' -f2
