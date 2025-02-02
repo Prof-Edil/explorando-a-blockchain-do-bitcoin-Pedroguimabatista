b14daec651b6f6beb8dbb610d1d7d570bab0028e
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-02 17:02:51 -0300

    Seventh Problem - seventh try

--- submission/007.sh
+++ submission/007.sh
@@ -1,40 +1,29 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 
-#!/usr/bin/env bash
 
-#!/bin/bash
-set -e
+hash123321=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblockhash "123321")
 
-block_hash=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblockhash 123321)
-block=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblock "$block_hash")
+blockdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblock $hash123321)
 
-unspent_count=0
-unspent_address=""
 
-for tx in $(echo "$block" | jq -r '.tx[]'); do
-    tx_data=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction "$tx" 1)
-    num_outputs=$(echo "$tx_data" | jq '.vout | length')
+transactions=$(echo $blockdata | jq -r '.tx[]')
 
-    for (( i=0; i<num_outputs; i++ )); do
-        txout=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" gettxout "$tx" "$i")
 
-        if [ "$txout" != "null" ]; then
-            addr=$(echo "$tx_data" | jq -r ".vout[$i].scriptPubKey.addresses[0]")
-
-            if [ "$addr" != "null" ] && [ -n "$addr" ]; then
-                unspent_address="$addr"
-                unspent_count=$((unspent_count+1))
-            fi
+while IFS= read -r tx; do
+    
+    txdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction "$tx" 1)
+    
+    
+    vout_count=$(echo $txdata | jq '.vout | length')
+    
+    for i in $(seq 0 $(($vout_count - 1))); do
+        unspent=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" gettxout "$tx" $i)
+        if [ ! -z "$unspent" ]; then
+            echo "Found unspent output:"
+            echo $txdata | jq -r ".vout[$i].scriptPubKey.address"
         fi
     done
-done
-
-if [ $unspent_count -eq 1 ]; then
-    echo "$unspent_address"
-else
-    echo "Erro: Encontrado $unspent_count outputs não gastos. Esperava exatamente 1." >&2
-    exit 1
-fi
+done <<< "$transactions"
 
 
 
