ebfe360eccd9644e060fc73cdd790d8efbcf3b05
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-01 21:04:57 -0300

    Seventh problem - Fourth try

--- submission/007.sh
+++ submission/007.sh
@@ -1,6 +1,5 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 
-# Only one single output remains unspent from block 123,321. What address was it sent to?
 
 hash123321=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblockhash "123321")
 blockdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblock $hash123321)
