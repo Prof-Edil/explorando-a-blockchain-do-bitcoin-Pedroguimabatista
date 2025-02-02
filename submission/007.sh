128b02bfdb65c5e5d211e82002fb8e69ddcb644e
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-01 21:16:01 -0300

    Seventh problem - Fifth try

--- submission/007.sh
+++ submission/007.sh
@@ -1,9 +1,24 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 
+#!/usr/bin/env bash
 
-hash123321=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblockhash "123321")
-blockdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getblock $hash123321)
-siginputtx=$(echo $blockdata | jq -r '.tx[6]')
-answer=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction "$siginputtx" 1)
-echo $answer | jq -r '.vout[0].scriptPubKey.address'
+hash123321=$(bitcoin-cli  -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp"  getblockhash 123321)
+blockdata=$(bitcoin-cli  -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp"  getblock "$hash123321")
+txids=$(echo "$blockdata" | jq -r '.tx[]')
+
+for txid in $txids; do
+rawtx=$(bitcoin-cli  -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp"  getrawtransaction "$txid" 1)
+vout_count=$(echo "$rawtx" | jq '.vout | length')
+
+for (( i=0; i<"$vout_count"; i++ )); do
+unspent=$(bitcoin-cli  -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp"  gettxout "$txid" "$i")
+if [ -n "$unspent" ]; then
+address=$(echo "$rawtx" | jq -r ".vout[$i].scriptPubKey.addresses[0] // .vout[$i].scriptPubKey.address // empty")
+echo "$address"
+exit 0
+fi
+done
+done
+
+
 
