212f0b106f6af6116b5f6e74424df369928b7e6c
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-02 13:43:36 -0300

    Eight problem - third try

--- submission/008.sh
+++ submission/008.sh
@@ -2,7 +2,7 @@
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
 
-rawtx=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1)
+rawtx=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163)
 npubencodeed=$(echo $rawtx | jq -r '.vin[0].txinwitness[2]')
 decode=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" decodescript $npubencodeed)
 echo $decode | jq -r '.asm' | cut -d ' ' -f2
