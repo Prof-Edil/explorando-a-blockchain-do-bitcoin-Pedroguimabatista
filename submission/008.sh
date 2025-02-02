82069e90062911bdcd6f44110e673e6fda8729f6
Pedro Batista <pedroguimabatista@gmail.com>
2025-02-01 21:28:35 -0300

    Eight problem

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,8 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+#!/usr/bin/env bash
+
+tx_data=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser="user_256" -rpcpassword="LSduUiqlO3wp" getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1)
+pubkey=$(echo "$tx_data" | jq -r '.vin[0].txinwitness[1]')
+echo "$pubkey"
